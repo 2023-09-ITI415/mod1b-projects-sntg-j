51a313ad50c04e06c68bb28d5f67f6e3ee253e97
sntg-j <sntgmatz@gmail.com>
2023-10-02 13:47:31 -0400

    Updates
    Staged and set the prefabs for the game.
    Created and implemented the following scripts:
    Slingshot for the projectile prefab
     - ProjectileLine for aiming (Bug fixes in progress)
     - FollowCam for projectile (Bug fixes in progress)

---
+++ Assets/02-Mission Demolition/Scripts/ProjectileLine.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileLine : MonoBehaviour
+{
+    static public ProjectileLine S;
+
+    [Header("Set in Inspector")]
+    public float minDist = 0.1f;
+
+    private LineRenderer line;
+    private GameObject _poi;
+    private List<Vector3> points;
+
+    void Awake()
+    {
+        S = this;
+        line = GetComponent<LineRenderer>();
+        line.enabled = false;
+        points = new List<Vector3>();
+    }
+
+    public GameObject poi
+    {
+        get { return (_poi); }
+        set { _poi = value;
+            if (_poi != null)
+            {
+                line.enabled = false;
+                points = new List<Vector3>();
+                AddPoint();
+            }
+        }
+    }
+
+    public void Clear()
+    {
+        _poi = null;
+        line.enabled = false;
+        points = new List<Vector3>();
+    }
+
+    public void AddPoint()
+    {
+        Vector3 pt = _poi.transform.position; 
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
